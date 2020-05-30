2f38cf9bad678a1dcacf0d4b5a22916823c6a71d
Asterisk <98jiminlee@gmail.com>
2020-05-30 12:31:55 +0900

    fd

--- recipe-server.js
+++ recipe-server.js
@@ -61,3 +61,16 @@ app.get('/recipe', (request, response)=>{
             })
     })
 })
+// Select ceil(count(*)/12.0) from recipe
+app.get('/recipe_total', (request, response)=>{
+    var url="mongodb://211.238.142.181:27017"
+    Client.connect(url, (err,client)=>{
+        var db = client.db('mydb');
+        // count() ==> return
+        db.collection('recipe').find({}).count((err, count)=>{
+            response.json({total:Math.ceil(count/12.0)})
+            client.close();
+            return count;
+        })
+    })
+})
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,80 @@
-import React from "react";
-
+import React,{useState, useEffect} from "react";
+import axios from 'axios'
+/*
+*  class Recipe
+*   {
+*       private String[] recipe;
+*       public void setRecipe(String[] recipe)
+*       {
+*           this.recipe=recipe
+*       }
+*   }
+* */
 export default function Recipe() {
-    return (
-        <div><h1>레시피</h1></div>
+    const [recipe, setRecipe]=useState([]);
+    const [page, setPage]=useState(1);
+    const [total, setTotal]=useState(0);
+    // http://localhost:3355/recipe?page=1
+    useEffect(async ()=>{
+        //서버를 연결해서 데이터를 읽어온 후 => setRecipe에 저장
+        await axios.get('http://localhost:3355/recipe', {
+            params:{
+                page:page
+            }
+        }).then((result)=>{
+            setRecipe(result.data);
+        })
+    }, [])
+
+    useEffect(async ()=>{
+        await axios.get('http://localhost:3355/recipe_total').then((result)=>{
+            setTotal(result.data.total)
+        })
+    },[])
+    // 출력할 데이터를 모아서 => return에 전송
+    const onPrev=()=>{
+        setPage(page>1?page-1:page)
+        axios.get('http://localhost:3355/recipe', {
+            params:{
+                page:page
+            }
+        }).then((result)=>{
+            setRecipe(result.data);
+        })
+    }
+    const onNext=()=>{
+        setPage(page<total?page+1:page)
+        axios.get('http://localhost:3355/recipe', {
+            params:{
+                page:page
+            }
+        }).then((result)=>{
+            setRecipe(result.data);
+        })
+    }
+    // render()
+    const html=recipe.map((m)=>
+        <div className="col-md-4">
+            <div className="thumbnail">
+
+                <img src={m.poster} alt="Lights" style={{"width":"100%"}}/>
+                <div className="caption">
+                    <p style={{"fontSize":"9pt"}}>{m.title}</p>
+                    <sub style={{"color":"gray"}}>{m.chef}</sub>
+                </div>
+            </div>
+        </div>
+    )
+   return (
+       <React.Fragment>
+           <div className={"row"}>
+               {html}
+           </div>
+           <div className={"row"}>
+               <button className={"btn btn-lg btn-primary"} onClick={onPrev}>이전</button>
+               {page} page / {total} pages
+               <button className={"btn btn-lg btn-danger"} onClick={onNext}>다음</button>
+           </div>
+       </React.Fragment>
     )
 }
